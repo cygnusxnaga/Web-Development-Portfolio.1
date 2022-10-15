83330bb81e225297ce188b63f9a79ad888c0343b
cygnusxnaga <105466828+cygnusxnaga@users.noreply.github.com>
2022-10-14 21:43:02 -0400

    Add files via upload

---
+++ proj.v/section5/app.js
@@ -0,0 +1,39 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const body = document.querySelector('body')
+
+    const img = document.getElementById('img');
+
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        body.classList.toggle('body');
+
+
+//animate links?? //TO DO
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 3}s';
+            }
+        });
+//burger animation
+            burger.classList.toggle('toggle');
+
+    });
+}
+
+
+
+
+
+
+
+navSlide();
+
+
+
+
